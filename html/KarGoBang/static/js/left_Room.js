1ffc491446acc3e5c4f873af3623628474363352
ZeYu Wang <lmq122677@qq.com>
2022-09-12 23:07:28 +0800

    页面优化:   适配了手机   更改了五子棋相关游戏地址   修复了点击bug

--- html/KarGoBang/static/js/left_Room.js
+++ html/KarGoBang/static/js/left_Room.js
@@ -2,6 +2,65 @@ var sex;
 var sexFlag;
 var username;
 var message;
+
+function modifyIcon() {
+
+    var inputObj=document.createElement('input')
+    inputObj.setAttribute('id','_ef');
+    inputObj.setAttribute('type','file');
+    inputObj.setAttribute("style",'visibility:hidden');
+    document.body.appendChild(inputObj);
+    var file;
+    $("#_ef").click().on('change',async event => {
+        file = event.target.files[0];
+        // let src = window.URL.createObjectURL(file)
+        // $(".img-user").attr("src", src);
+        upload(file);
+
+    })
+}
+
+function upload(file) {
+    const isImage = (file.type === 'image/jpeg' || file.type === 'image/png' || file.type === 'image/gif');
+    const isLimit = file.size / 1024 / 512 < 1;
+    if (!isImage) {
+        window.alert('上传文件只能是图片格式!')
+        return false;
+    }
+    if (!isLimit)
+    {
+        window.alert('上传文件大小不能超过 500 KB!')
+        return false;
+    }
+    var reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => {
+        $.ajax({
+            type: "post",
+            url: '/api/user/uploadIcon',
+            // cache: false,
+            dataType: "json",
+            data: {
+                file:reader.result
+            },
+            xhrFields: {
+                withCredentials: true,
+            },
+            success: function (data) {
+                if (data.code === 200) {
+                    $(".img-user").attr("src", reader.result);
+
+                } else {
+                    window.alert(data.message)
+                }
+            },
+            error: function () {
+                window.alert("服务器未连接,修改失败,请联系管理员");
+            },
+        });
+    };
+}
+
 $(function () {
     $(".card-user").css("visibility","visible");
     $("#card-user").hide();
@@ -99,18 +158,26 @@ function saveInfo() {
     let inputName = $(".wrapper-input").val();
     let inputMessage = $(".wrapper-input-2").val();
     let inputSex = $("#checkbox-sex").text();
-    if (username === inputName && message === inputMessage && sex === inputSex) return;
+    if (username === inputName && message === inputMessage && sex === inputSex) {
+        window.alert("跟原用户信息无变化!");
+        return;
+    }
     let formatData = {};
-    if (username !== inputName) {
+    if (inputName === "" && inputMessage === ""&&sex === inputSex) {
+        window.alert("内容不能为空!");
+        return;
+
+    }
+    if (username !== inputName && inputName !== ""&& inputName !== null) {
         formatData['username'] = inputName;
     }
-    if (message !== inputMessage) {
+    if (message !== inputMessage && inputMessage !== ""&& inputMessage !== null) {
         formatData['message'] = inputMessage;
     }
     if (sex !== inputSex) {
-        formatData['sex'] = inputMessage;
+        formatData['sex'] = inputSex;
     }
-    if (username !== inputName && message !== inputMessage && sex !== inputSex) {
+    if (username !== inputName && message !== inputMessage && sex !== inputSex&&inputName !==""&&inputMessage!==""&& inputSex !== "") {
         $.ajax({
             type: "put",
             url: '/api/user/modifyUserDetails',
@@ -122,7 +189,13 @@ function saveInfo() {
             },
             success: function (data) {
                 if (data.code === 200) {
-                    getUserDetails();
+                    if (username !== inputName&&inputName!==""&&inputName!==null) {
+                        logout();
+                        window.location = '/login.html';
+                        return;
+                    }
+                    $.ajaxSetup ({cache:false});
+                    getUserDetailsNoCache();
                 } else if (data.code === 401) {
                     window.location = "/login.html";
                 } else if (data.code !== 200) {
@@ -146,7 +219,13 @@ function saveInfo() {
         },
         success: function (data) {
             if (data.code === 200) {
-                getUserDetails();
+                if (username !== inputName&&inputName!==""&&inputName!==null) {
+                    logout();
+                    window.location = '/login.html';
+                    return;
+                }
+                $.ajaxSetup ({cache:false});
+                getUserDetailsNoCache();
             } else if (data.code === 401) {
                 window.location = "/login.html";
             } else if (data.code !== 200) {
@@ -160,7 +239,92 @@ function saveInfo() {
 }
 
 window.onload = getUserDetails();
+function getUserDetailsNoCache() {
+    $.ajax({
+        type: 'get',
+        url: '/api/user/getUserDetails',
+        async: false,
+        data: {},
+        dataType: 'json',
+        xhrFields: {
+            withCredentials: true,
+        },
+        headers: {
+            'Cache-Control':'no-cache',
+        },
+        cache:false,
+        success: function (data) {
+            if (data.code === 200) {
+                username = data.data.username;
+                message = data.data.userInfo.message;
 
+                if (data.data.userInfo.sex === "男") {
+                    sexFlag = "fa fa-mars i-user";
+                } else {
+                    sexFlag = "fa fa-venus i-user";
+                }
+
+                $(".content-user").html("<div class=\"content-user\">\n" +
+                    "        <div class=\"details-user\">\n" +
+                    "            <h2 id=\"card-name\" class=\"h2-user\">" + data.data.username + "\n" +
+                    "                <i id=\"card-sex\" class=\"" + sexFlag + "\" aria-hidden=\"true\"></i>\n" +
+                    "                <br />\n" +
+                    "                <span id=\"card-message\" class=\"span-user\">"+message+"</span></h2>\n" +
+                    "            <div class=\"data\">\n" +
+                    "                <h3 id=\"card-data1\" class=\"h3-user\">\n" +
+                    "                    0\n" +
+                    "                    <br />\n" +
+                    "                    <span class=\"span-user\">游戏局数</span>\n" +
+                    "                </h3>\n" +
+                    "                <h3 id=\"card-data2\" class=\"h3-user\">\n" +
+                    "                    0%\n" +
+                    "                    <br />\n" +
+                    "                    <span class=\"span-user\">游戏胜率</span>\n" +
+                    "                </h3>\n" +
+                    "                <h3 id=\"card-data3\" class=\"h3-user\">\n" +
+                    "                    0\n" +
+                    "                    <br />\n" +
+                    "                    <span class=\"span-user\">熄灯关数</span>\n" +
+                    "                </h3>\n" +
+                    "            </div>\n" +
+                    "            <div class=\"actionBtn\">\n" +
+                    "                <button id=\"card-button1\" class=\"button-user\" type=\"button\" onclick=\"modifyInfo()\">登录用户</button>\n" +
+                    "                <button id=\"card-button2\" class=\"button-user\" type=\"button\" onclick=\"modifyIcon()\">注册用户</button>\n" +
+                    "            </div>\n" +
+                    "        </div>\n" +
+                    "    </div>")
+                sex = data.data.userInfo.sex;
+                if (data.data.userInfo.sex === "男") {
+                    $("#card-sex").css("color", "#0679a2");
+                    if (data.data.userInfo.icon===null)
+                        $(".img-user").attr("src", "static/images/boy.png");
+                }
+                if (data.data.userInfo.icon!==null) {
+                    icon = data.data.userInfo.icon;
+                    $(".img-user").attr("src", icon);
+                }
+
+                $("#card-button1").attr("onClick", "modifyInfo()")
+                $("#card-message").text(data.data.userInfo.message);
+                $("#card-data1").html(data.data.userScore.wins + "<br />\n" +
+                    "                    <span class=\"span-user\">游戏局数</span>");
+                $("#card-data2").html(data.data.userScore.winRate + '%' + "<br />\n" +
+                    "                    <span class=\"span-user\">游戏胜率</span>");
+                $("#card-data3").html(data.data.userScore.passNumber + "<br />\n" +
+                    "                    <span class=\"span-user\">熄灯关数</span>");
+                $("#card-button1").text("修改信息");
+                $("#card-button2").text("修改头像");
+                // $(".imgBox").css("transform","translate(50%)")
+
+
+            } else if (data.code === 401) {
+                //未登录
+            } else if (data.code !== 200) {
+                //未登录
+            }
+        },
+    })
+}
 function getUserDetails() {
     $.ajax({
         type: 'get',
@@ -279,7 +443,7 @@ function logout() {
             }
         },
         error: function (data) {
-            window.location = "/500page.html";
+            window.location = "/home.html";
         },
     })
 }
